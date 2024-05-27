a9818de7634dffb40e5b5d0d573daaf099d63a5d
turntable-justin <122627527+turntable-justin@users.noreply.github.com>
2024-05-27 13:38:02 -0500

    use example theme

---
+++ theme.ts
@@ -0,0 +1,17 @@
+const colors = require("tailwindcss/colors");
+
+export default {
+  darkMode: "selector",
+  theme: {
+    extend: {
+      colors: {
+        black: "#000000",
+        white: "#ffffff",
+        primary: colors.fuchsia,
+        "page-background": "#ffffff",
+        "card-border": "#000000",
+        "card-background": "#ffffff",
+      },
+    },
+  },
+};
